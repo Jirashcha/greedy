387748e23732e1ef8ab1c16f922262e22ccd3e81
Jirashcha <120640294+Jirashcha@users.noreply.github.com>
2023-10-27 22:52:49 +0700

    Add files via upload

--- greedyActivitySelection.py
+++ greedyActivitySelection.py
@@ -1,16 +1,12 @@
 #Greedy Algorithm: Activity Selection (Activity n activity and room k room. Output; wants all events to be held with minimal room)
+#Jirashcha Wanggum 1640702609 CS441 sec. 327E
 
 act = int(input("Enter number of activities: "))
-room = int(input("Enter number of rooms: "))
-actList = []
-roomList = []
-countActSelected = 0
-countRoomSelected = 0
-
-if room and act >= 1:
-    for i in range(room):
-        r = {"room":i+1,":":[]}
-        roomList.append(r)
+if act >= 1:
+    actList = []
+    roomList = []
+    countAct = 0
+    k = 0
 
     for i in range(act):
         print("Enter start and finish time for activity", i+1, ":")
@@ -29,26 +25,26 @@ if room and act >= 1:
                 actList[j] = actList[j+1]
                 actList[j+1] = temp
     
-    for i in range(len(actList)):
-        for j in range(len(roomList)):
-            if len(roomList[j][":"]) == 0:
-                roomList[j][":"].append(actList[i])
-                countActSelected += 1
-                countRoomSelected += 1
+    while len(actList) != countAct:
+        r = {"room":k+1,":":[]}
+        roomList.append(r)
+        for i in range(len(actList)):
+            if len(roomList[k][":"]) == 0:
+                roomList[k][":"].append(actList[i])
+                countAct += 1
                 break
             else:
-                last = len(roomList[j][":"]) - 1
-                if actList[i]["start"] >= roomList[j][":"][last]["finish"]:
-                    roomList[j][":"].append(actList[i])
-                    countActSelected += 1
+                last = len(roomList[k][":"]) - 1
+                if actList[i]["start"] >= roomList[k][":"][last]["finish"]:
+                    roomList[k][":"].append(actList[i])
+                    countAct += 1
                     break
-    
+        k += 1
+        
+        
     print("------------------------------------")
-    if countActSelected != len(actList):
-        print("Not all activities can be selected.")
-    else:
-        print("All activities can be selected.")
-        print("The minimal number of rooms possible is:", countRoomSelected)  
+    print("All activities can be selected.")
+    print("The minimal number of rooms possible is:", k)  
     print("The activities selected in each room are:")       
     for i in range(len(roomList)):
         if len(roomList[i][":"]) != 0:
@@ -61,4 +57,4 @@ if room and act >= 1:
     print("------------------------------------")
     
 else:
-    print("Invalid input. Number of rooms and activities must be greater than 0.")
+    print("Invalid input. Number of activities must be greater than 0.")
